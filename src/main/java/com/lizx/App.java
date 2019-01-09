dc2bb5c63632b06b6f710112a61d2b17106c2363
lizexing1 <39845355+lizexing1@users.noreply.github.com>
2019-01-09 14:16:04 +0800

    sssssssssss

--- src/main/java/com/lizx/App.java
+++ src/main/java/com/lizx/App.java
@@ -9,6 +9,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println("11111ssssssssssss1");
+        
         System.out.println( "Hello World!" );
     }
 }
