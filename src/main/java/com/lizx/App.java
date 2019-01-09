1e50542c595adadf8c9c289c6e1557135bedbf67
lizexing1 <39845355+lizexing1@users.noreply.github.com>
2019-01-09 14:13:02 +0800

    Update App.java

--- src/main/java/com/lizx/App.java
+++ src/main/java/com/lizx/App.java
@@ -8,7 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println("111111");
+        System.out.println("11111ssssssssssss1");
         System.out.println( "Hello World!" );
     }
 }
